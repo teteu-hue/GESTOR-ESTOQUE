28f83c4f07245103d058496d44fd41c00f55f87b
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-04-19 16:00:09 -0300

    Adicionar arquivos de projeto.

---
+++ GESTOR-ESTOQUE/Class/Fornecedor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GESTOR_ESTOQUE.Class
+{
+    class Fornecedor : Pessoa
+    {
+        private string cnpj;
+        private string razaoSocial;
+        private string nomeFantasia;
+
+        public Fornecedor() : base()
+        {   
+        }
+
+        public Fornecedor(string cnpj) : base()
+        {
+            this.cnpj = cnpj;
+        }
+
+        public Fornecedor(string cnpj, string razaoSocial) : base()
+        {
+            this.cnpj = cnpj;
+            this.razaoSocial = razaoSocial;
+        }
+
+        public Fornecedor(string cnpj, string razaoSocial, string nomeFantasia) : base()
+        {
+            this.cnpj = cnpj;
+            this.razaoSocial = razaoSocial;
+            this.nomeFantasia = nomeFantasia;
+        }
+    }
+}
---
+++ GESTOR-ESTOQUE/Class/Pessoa.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GESTOR_ESTOQUE.Class
+{
+    class Pessoa
+    {
+        private int pessoaID;
+        private string nome;
+        private string email;
+        private string telefone;
+        private string endereco;
+
+        public Pessoa()
+        {
+        }
+
+        public Pessoa(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public Pessoa(string nome, string email)
+        {
+            this.nome = nome;
+            this.email = email;
+        }
+
+        public Pessoa(string nome, string email, string telefone)
+        {
+            this.nome = nome;
+            this.email = email;
+            this.telefone = telefone;
+        }
+
+        public Pessoa(string nome, string email, string telefone, string endereco)
+        {
+            this.nome = nome;
+            this.email = email;
+            this.telefone = telefone;
+            this.endereco = endereco;
+        }
+
+    }
+}
